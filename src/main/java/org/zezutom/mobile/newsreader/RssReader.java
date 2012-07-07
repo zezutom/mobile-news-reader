2ddaa8600658c47d04ba506bf51b797fce2035d7
zezutom <zezulatomas@gmail.com>
2012-07-08 01:41:17 +0200

    Core features implemented. Transition between the home page, the category selection and news list completed.

--- src/main/java/org/zezutom/mobile/newsreader/RssReader.java
+++ src/main/java/org/zezutom/mobile/newsreader/RssReader.java
@@ -7,10 +7,12 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
 import com.sun.syndication.feed.synd.SyndEntry;
 import com.sun.syndication.feed.synd.SyndFeed;
@@ -20,70 +22,78 @@ import com.sun.syndication.io.XmlReader;
 @ManagedBean
 @SessionScoped
 public class RssReader {
-	
-	public static final String FEED_URL = "http://rss.news.yahoo.com/rss";
-	
-	public static final int MAX_COUNT = 10;
-	
-	public static final String DATE_FORMAT = "EEEE MMMM dd, yyyy HH:mm:ss";
 
+	public static final String FEED_URL = "http://rss.news.yahoo.com/rss/";
+
+	public static final int MAX_COUNT = 10;
+
+	public static final String DATE_FORMAT = "EEEE MMMM dd, yyyy HH:mm:ss";
+	
 	private String category;
+
+	public void setCategory(String category) {		
+		this.category = category;
+	}
 	
 	public String getCategory() {
 		return category;
 	}
 
-	public void setCategory(String category) {
-		this.category = category;
-	}
-
 	public List<RssEntry> getFeeds() {
-		return getFeeds(FEED_URL, MAX_COUNT);
+		return getFeeds(getFeedUrl(category), MAX_COUNT);
 	}
 	
+	private String getFeedUrl(String category) {
+		StringBuilder sb = new StringBuilder(FEED_URL);
+
+		if (category != null && !category.isEmpty()) {
+			sb.append(category);
+		}
+
+		return sb.toString();
+	}
+
 	private List<RssEntry> getFeeds(String url, int count) {
 		List<RssEntry> feeds = new ArrayList<RssEntry>();
-		
+
 		DateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
-		
+
 		try {
 			URLConnection feedUrl = new URL(url).openConnection();
 			SyndFeedInput input = new SyndFeedInput();
-			
+
 			// Populate data
 			SyndFeed feed = input.build(new XmlReader(feedUrl));
-			
+
 			List feedList = feed.getEntries();
 			int size = feedList.size();
-			
+
 			if (size > count) {
 				size = count;
 			}
-			
-			for (int i=0; i<size; i++) {
+
+			for (int i = 0; i < size; i++) {
 				SyndEntry syndEntry = (SyndEntry) feedList.get(i);
-				
+
 				// Custom format
 				RssEntry rssEntry = new RssEntry();
-				
+
 				rssEntry.setLink(syndEntry.getLink());
 				rssEntry.setTitle(syndEntry.getTitle());
 				rssEntry.setAuthor(syndEntry.getAuthor());
 				rssEntry.setDescription(syndEntry.getDescription().getValue());
-				
+
 				Date published = syndEntry.getPublishedDate();
 				rssEntry.setPublishedDate(dateFormat.format(published));
-				
+
 				feeds.add(rssEntry);
-				
 			}
-			
+
 		} catch (Exception ex) {
 			feeds.add(RssEntry.createErrorEntry(ex));
 		}
 		
 		return feeds;
 	}
-	
 
 }
--- src/main/webapp/js-css/newsreader.js
+++ src/main/webapp/js-css/newsreader.js
@@ -1,26 +1,5 @@
-var NEWS_URL = 'news';
-
-function init() {
-	$("#add_category_button").bind('click', function() {
-		
-		var result = false;
-		var category = $("#category_select").val();
-		
-		if(category != '') {
-			result = getNews(category, addNews)
-		}
-
-		return result;
-	});
-}
-
-function addNews(json) {
-	alert('json');
-	$("#home").show();
-}
-
-function getNews(category, handler) {
-	var url = NEWS_URL + '?category=' category;
-	$.ajax({type: 'GET', dataType: 'json', url: url, success: handler});
-	return false;
+function showNews(e) {
+	if (e.status == 'success') {
+		$.mobile.changePage('news.htm');
+	}	
 }
